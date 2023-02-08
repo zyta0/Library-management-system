7d2a731bf98bcda7397ca45c88a8fda182650935
zyt <948596698@qq.com>
2023-02-08 16:04:31 +0800

    首页轮播图

--- front/JS/index.js
+++ front/JS/index.js
@@ -1,5 +1,6 @@
 const logReg = document.getElementById("login")
-
+const carouselMapPic = document.getElementsByClassName("carouselMapPic")[0]
+const lisLi = document.querySelectorAll(".lis li")
 function checkState() {
     const tokenStr = localStorage.getItem("token");
     const token = JSON.parse(tokenStr)
@@ -13,3 +14,44 @@ function checkState() {
     }
 }
 checkState()
+
+function addRem(num) {
+    for (i = 0; i < 3; i++) {
+        if (num == i) {
+            lisLi[i].classList.add("lisSelect")
+        } else {
+            lisLi[i].classList.remove("lisSelect")
+        }
+    }
+}
+
+function circleSel(){
+    
+        switch (carouselMapPic.offsetLeft) {
+            case 0:
+                addRem(1)
+                break;
+            case -850:
+                addRem(2)
+                break;
+            case -1700:
+                addRem(0)
+                break;
+        }
+    
+}
+
+function animate() {
+    let step = 850
+    if (carouselMapPic.offsetLeft == -1700) {
+        carouselMapPic.style.left = 0 + "px"
+    } else {
+        carouselMapPic.style.left = carouselMapPic.offsetLeft - step + "px"
+    }
+}
+
+
+ setInterval(() => {
+    animate()
+     circleSel()
+ },3000)
