4d5c92b812d7f78211cb09a23f8663d4d9acde77
shiluyi <574940748@qq.com>
2023-03-22 17:43:21 +0800

    书库页面的请求

--- front/JS/stackRoom.js
+++ front/JS/stackRoom.js
@@ -8,3 +8,28 @@
                 classificationDetails[i].className = "signed";
             }
         }
+        
+//书库页面的请求
+        let bookPic = document.querySelectorAll(".bookPic>img");
+        let bookName = document.querySelectorAll(".bookName");
+        axios({
+                method: "post",
+                url: "http://frp-fly.top:57378/bookServlet"
+            })
+                .then((result) => {
+                    console.log(result.data);
+                    console.log(result.data[0].name);
+                    console.log(result.data[1].url);
+                    console.log(result.data[2].url);
+                    for (let i = 0; i < result.data.length; i++) {
+                        bookName[i].innerText = result.data[i].name;
+                        bookPic[i].src = `${result.data[i].url}`;
+                        if (result.data[i].url.includes(".png")) {
+                            bookPic[i].src = `${result.data[i].url}`;
+                            console.log(bookPic[i].src);
+                        }
+                    }
+                })
+                .catch((err) => {
+                    console.log("出错了！", err)
+                })
